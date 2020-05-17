44e63f89a482e649da5b98e9641561beeb1f7cb2
cveral <cavera2@miuandes.cl>
2020-05-17 18:17:20 -0400

    first commit

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
